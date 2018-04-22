179ae015a25c343884630d496d66448fe998a513
Theo Pack <tf.pack@gmail.com>
2018-04-22 10:53:23 +0200

    Add kafka listener

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -10,21 +12,72 @@ import (
 
 func topic(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	fmt.Printf("URL %s", vars["topic"])
 	flusher, ok := w.(http.Flusher)
 	if !ok {
 		panic("expected http.ResponseWriter to be an http.Flusher")
 	}
-
 	w.Header().Set("Connection", "Keep-Alive")
 	w.Header().Set("Transfer-Encoding", "chunked")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 
-	for i := 1; i <= 10; i++ {
-		fmt.Fprintf(w, "Chunk #%d\n", i)
+	closeNotify := w.(http.CloseNotifier).CloseNotify()
+	events := make(chan string)
+
+	go runKafkaListener(vars["topic"], closeNotify, events)
+
+	for {
+		event, ok := <-events
+		if !ok {
+			return
+		}
+		fmt.Fprintf(w, "%s\n", event)
 		flusher.Flush()
-		time.Sleep(500 * time.Millisecond)
 	}
+}
+
+func runKafkaListener(topic string, closeNotify <-chan bool, events chan string) {
+
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"group.id":          "myGroup" + pseudoUUID(),
+		"auto.offset.reset": "latest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	c.SubscribeTopics([]string{topic}, nil)
+	connectionClosed := false
+
+	go func() {
+		<-closeNotify
+		close(events)
+		c.Close()
+		connectionClosed = true
+	}()
+
+	for {
+		msg, err := c.ReadMessage(10 * time.Second)
+		if err == nil {
+			events <- string(msg.Value)
+		} else {
+			if connectionClosed {
+				break
+			}
+		}
+	}
+}
+
+func pseudoUUID() string {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return ""
+	}
+
+	return fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 }
 
 func main() {
